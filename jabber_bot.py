3d3b93c77b810cdc504059dee32c272558df4434
A. Maslennikov <amasl2048@gmail.com>
2015-03-22 23:44:12 +0300

    add regex for ping, urlinfo, nslookup, whois

--- jabber_bot.py
+++ jabber_bot.py
@@ -3,20 +3,88 @@
 from jabberbot import JabberBot, botcmd
 import datetime
 import yaml
-import sys
+import sys, subprocess, re
 from com.yahoo_weather import weather_report as weather
 from com.rbc_currency import rbc_get as rbc
 from com.bbc_news import bbc_rss as bbc
 from com.micex import ex_rate as rate
 
 class SystemInfoJabberBot(JabberBot):
+
+    def check_url( self, arg):
+        r = re.compile(r"^[a-z0-9_.-]+$")
+        if ( (len(arg) < 256) and (r.search(arg.strip())) ):
+            return True
+        return False
+        
     @botcmd
     def serverinfo( self, mess, args):
         """Displays information about the server"""
         version = open('/proc/version').read().strip()
         loadavg = open('/proc/loadavg').read().strip()
-
         return '%s\n\n%s' % ( version, loadavg, )
+
+    @botcmd
+    def uptime( self, mess, args):
+        """Displays uptime of the server"""
+        p = subprocess.Popen(['uptime', '-p'],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        return str(out)
+
+    @botcmd
+    def urlinfo( self, mess, args):
+        """Displays information from url"""
+        if (self.check_url(args)):
+            s = str(args).strip()
+        else:
+            return "Error"
+        p = subprocess.Popen(['curl', '-I', s],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        return str(out)
+
+    @botcmd
+    def ping( self, mess, args):
+        """Ping address"""
+        if (self.check_url(args)):
+            s = str(args).strip()
+        else:
+            return "Error"
+        p = subprocess.Popen(['ping', '-c 3', s],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if err: return str(err)
+        return str(out)
+
+    @botcmd
+    def nslookup( self, mess, args):
+        """DNS name look up resolution"""
+        if (self.check_url(args)):
+            s = str(args).strip()
+        else:
+            return "Error"
+        p = subprocess.Popen(['nslookup', s],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        return str(out)
+
+    @botcmd
+    def whois( self, mess, args):
+        """Whois adrress service"""
+        if (self.check_url(args)):
+            s = str(args).strip()
+        else:
+            return "Error"
+        p = subprocess.Popen(['whois', s],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        return str(out)
 
     @botcmd
     def time( self, mess, args):
@@ -26,6 +94,7 @@ class SystemInfoJabberBot(JabberBot):
     @botcmd
     def rot13( self, mess, args):
         """Returns passed arguments rot13'ed"""
+        if (len(args) > 255): return "Too long args..."
         return args.encode('rot13')
 
     @botcmd
