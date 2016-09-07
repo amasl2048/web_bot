1dcc989ce3a7786ac7f9bf62ad84de85745ced5e
Andrey Maslennikov <amasl2048@gmail.com>
2016-09-07 18:50:42 +0200

    static_url logging fix

--- static_url.py
+++ static_url.py
@@ -6,10 +6,12 @@ from twisted.web.static import File
 import os, base64
 from random import choice
 import jabber_ru
-import yaml
+#import yaml
 
 import qrcode
 import sys
+
+import com.common as common
 '''
 Open web access to local file dir with random url:port
 >static_url.py <sub_dir> <days> &
@@ -17,29 +19,27 @@ Open web access to local file dir with random url:port
 and then send the link over jabber and generate QR-code
 '''
 
-# read config
-config = yaml.load(open("/etc/bot.config"))
-log_file = config["log_file"]
+log_file = common.config["log_file"]
 sys.stdout = open(log_file, "a")
 
-debug = config["debug"]
+debug = common.config["debug"]
 if debug:
     from twisted.python import log
     log.startLogging(open(log_file, "a"))
 
-host =       config["static_url"]["host"].strip()
-local_path = config["static_url"]["local"].strip() # default values
-days =       config["static_url"]["days"]
+host =       common.config["static_url"]["host"].strip()
+local_path = common.config["static_url"]["local"].strip() # default values
+days =       common.config["static_url"]["days"]
 
 url = base64.b32encode(str(os.urandom(10))).strip()
 
 if len(sys.argv) == 4: # becaurse "&" - the 4th argv
     if not sys.argv[1].isalnum(): # "/" and ".." in path is not permitted!
-        print "Error", sys.argv[1]
+        common.prnt_log("Error %s" % sys.argv[1])
         sys.exit(0)
     local_path = os.path.join(local_path, sys.argv[1])
     if not sys.argv[2].isdigit():
-        print "Error", sys.argv[2]
+        common.prnt_log("Error %s" % sys.argv[2])
         sys.exit(0)
     days = int(sys.argv[2])
 
@@ -59,9 +59,9 @@ root.putChild(url, File(local_path))
 factory = Site(root)
 reactor.listenTCP(port, factory)
 timer_sec = 60 * 60 * 24 * days # stop service after (sec * min * hours * days)
-reactor.callLater(timer_sec, reactor.stop)
-print "Start...", out, local_path, days
-reactor.run()
-print "Done."
 
+common.prnt_log("Starting... %s %s %s" % (out, local_path, days))
+reactor.callLater(timer_sec, reactor.stop)
+reactor.run()
+common.prnt_log("Reactor stop: %s %s %s" % (out, local_path, days))
 
