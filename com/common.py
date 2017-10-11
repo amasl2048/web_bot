53a829d10ac55324bfda9c1de5f7556cf50de20f
amasl2048 <amasl2048@users.noreply.github.com>
2017-10-11 21:31:30 +0200

    io.open encoding

--- com/common.py
+++ com/common.py
@@ -3,6 +3,7 @@ Common part
 '''
 import os
 import time
+import io
 import yaml
 
 config = yaml.load(open(os.environ["BOT_CFG"]))  #"/etc/bot.config"))
@@ -10,5 +11,5 @@ log_file = os.path.join(config["work_dir"], config["log_file"])
 
 def prnt_log(msg):
     '''Loging function'''
-    with open(log_file, "a", encoding='utf8') as f:
+    with io.open(log_file, "a", encoding='utf8') as f:
         f.write("[%s] %s\n" % (time.strftime("%c"), msg))
